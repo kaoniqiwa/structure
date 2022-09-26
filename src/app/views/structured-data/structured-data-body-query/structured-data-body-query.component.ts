770dc70983abc3db75fca77518def26f64660f82
tuzhongyi <tuzhongyi@howell.net.cn>
2022-09-26 17:31:29 +0800

    结构化

--- src/app/views/structured-data/structured-data-body-query/structured-data-body-query.component.ts
+++ src/app/views/structured-data/structured-data-body-query/structured-data-body-query.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { DateTimePickerView } from 'src/app/directives/date-time-picker.directive';
+import { Duration } from 'src/app/models/duration.model';
+import { DateTimeTool } from 'src/app/tools/datetime.tool';
 
 @Component({
   selector: 'app-structured-data-body-query',
   templateUrl: './structured-data-body-query.component.html',
-  styleUrls: ['./structured-data-body-query.component.less']
+  styleUrls: ['./structured-data-body-query.component.less'],
 })
 export class StructuredDataBodyQueryComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {}
+  DateTimePickerView = DateTimePickerView;
+  duration: Duration = DateTimeTool.allDay(new Date());
+  ngOnInit(): void {}
+  changebegin(date: Date) {
+    this.duration.begin = date;
   }
-
+  changeend(date: Date) {
+    this.duration.end = date;
+  }
 }
